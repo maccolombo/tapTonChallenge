68b6885e7ecf44c1690db204ce71d44a03efc8c0
Marcelo Colombo <marcelo.colombo@bredas.com.br>
2022-03-11 06:59:57 -0300

    release

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { navigationRef } from './routes/rootNavigation';
 import Routes from "./routes/routes";
 import { Provider } from 'react-redux';
 import store from './store';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer ref={navigationRef}>
+        <StatusBar backgroundColor={"green"} />
         <Routes />
       </NavigationContainer>
     </Provider>
